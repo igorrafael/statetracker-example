f70d85589c2c5516a8cefd29511b50ba33b5ce80
Igor Rafael de Sousa <igorrafaeldesousa@gmail.com>
2017-09-16 04:28:37 -0400

    Sample usage of TrackableBehaviour<Enum>

---
+++ Assets/example/KeyCodeTrackable.cs
@@ -0,0 +1,15 @@
+﻿using StateTracker;
+using UnityEngine;
+
+public class KeyCodeTrackable : TrackableBehaviour<KeyCode> {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
