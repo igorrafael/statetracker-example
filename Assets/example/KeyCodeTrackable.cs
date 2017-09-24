2e0ecf0a1019c40b4fc043dedfa0165e79b36a36
Igor Rafael de Sousa <igorrafaeldesousa@gmail.com>
2017-09-24 15:18:16 -0400

    Sandbox scene

--- Assets/example/KeyCodeTrackable.cs
+++ Assets/example/KeyCodeTrackable.cs
@@ -1,15 +1,27 @@
 ﻿using StateTracker;
+using System;
 using UnityEngine;
 
-public class KeyCodeTrackable : TrackableBehaviour<KeyCode> {
+public class KeyCodeTrackable : TrackableBehaviour<KeyCode>
+{
+    private KeyCode _lastKey;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public override KeyCode CurrentState
+    {
+        get
+        {
+            return _lastKey;
+        }
+    }
+
+    void Update()
+    {
+        foreach (KeyCode key in Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(key))
+            {
+                _lastKey = key;
+            }
+        }
+    }
 }
